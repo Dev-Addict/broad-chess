3d92fd74bdd867575707152666d26f1d492e276f
Aria AzadiPour <aazadipour@pm.me>
2024-07-05 20:33:17 -0230

    added valid moves for rooks

--- src/lib/utils/get-available-moves/get-available-moves.util.ts
+++ src/lib/utils/get-available-moves/get-available-moves.util.ts
@@ -1,6 +1,7 @@
 import {getAvailablePawnMoves} from './get-available-pawn-moves.util';
 import {getAvailableKnightMoves} from './get-available-knight-moves.util';
 import {getAvailableBishopMoves} from './get-available-bishop-moves.util';
+import {getAvailableRookMoves} from './get-available-rook-moves.util';
 import {ChessPieceType} from '../../types/enum/chess-piece-type.enum';
 import type {ChessBoard} from '../../types/chess-board/chess-board.type';
 import type {ChessPiecePosition} from '../../types/chess-piece/chess-piece-position.type';
@@ -20,6 +21,8 @@ export const getAvailableMoves = (
 			return getAvailableKnightMoves(chessBoard, position, piece);
 		case ChessPieceType.BISHOP:
 			return getAvailableBishopMoves(chessBoard, position, piece);
+		case ChessPieceType.ROOK:
+			return getAvailableRookMoves(chessBoard, position, piece);
 		default:
 			return [];
 	}
--- src/lib/utils/get-available-moves/get-available-rook-moves.util.ts
+++ src/lib/utils/get-available-moves/get-available-rook-moves.util.ts
@@ -3,14 +3,14 @@ import type {ChessBoard} from '../../types/chess-board/chess-board.type';
 import type {ChessPiecePosition} from '../../types/chess-piece/chess-piece-position.type';
 import type {ChessPiece} from '../../types/chess-piece/chess-piece.type';
 
-export const getAvailableBishopMoves = (
+export const getAvailableRookMoves = (
 	chessBoard: ChessBoard,
 	position: ChessPiecePosition,
 	piece: ChessPiece
 ): ChessPiecePosition[] =>
 	getAvailableDirectionalMoves(chessBoard, position, piece, [
-		[1, 1],
-		[1, -1],
-		[-1, 1],
-		[-1, -1],
+		[0, 1],
+		[0, -1],
+		[1, 0],
+		[-1, 0],
 	]);
